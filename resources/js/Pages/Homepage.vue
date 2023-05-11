c2aca3e2b1a8d37f90264af1e705f2f5c9137819
Benjamin Fontana <ben192003@gmail.com>
2023-05-11 13:38:29 +0200

    Added responsive on the homepage
    - Updated a few icons needed a refactor to let change the size of them
    - Updated the homepage to be responsive with the pc size

--- resources/js/Pages/Homepage.vue
+++ resources/js/Pages/Homepage.vue
@@ -10,6 +10,7 @@ import GasIcon from '@/Components/icons/GasIcon.vue';
 import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue';
 import { computed } from 'vue';
 import { useToast } from 'vue-toastification';
+import { PlusCircleIcon } from '@heroicons/vue/20/solid';
 
 const user = usePage().props.auth.user;
 const toast = useToast();
@@ -44,21 +45,21 @@ if (message) {
             <div class="relative z-50 flex justify-between w-full px-8 pt-8">
                 <SideBarMenu />
 
-                <div class="w-12 h-12">
+                <div class="w-12 h-12 md:w-24">
                     <img
                         :src="`/image/user/${user.id}`"
                         class="object-cover w-20 overflow-hidden rounded-full aspect-square" />
                 </div>
             </div>
             <div class="flex items-center justify-between">
-                <h1 class="pt-10 pl-12">Bonjour,<br />{{ user.username }}</h1>
+                <h1 class="pt-10 pl-12 md:text-4xl">Bonjour,<br />{{ user.username }}</h1>
 
                 <Link
                     v-if="rcModels.length > 0"
                     href="/rc-models/create"
                     class="flex items-center gap-2 mt-10 mr-8">
-                    <PlusIcon />
-                    <p>Créer</p>
+                    <PlusCircleIcon class="w-5 md:w-10" />
+                    <p class="md:text-4xl">Créer</p>
                 </Link>
             </div>
         </div>
@@ -76,48 +77,57 @@ if (message) {
                 </Link>
             </div>
         </div>
-        <div
-            v-for="rcModel in rcModels"
-            class="py-10 px-5 mx-2 shadow-lg h-[14rem] rounded-[3rem] last:mb-4">
-            <div class="flex justify-between py-2">
-                <Link
-                    as="h1"
-                    :href="'/rc-models/' + rcModel.id"
-                    class="max-w-full text-xl truncate text-app"
-                    >{{ rcModel.name }}</Link
-                >
-            </div>
-            <Link as="div" :href="'/rc-models/' + rcModel.id" class="flex justify-between">
-                <div class="aspect-square w-32 rounded-[2rem] overflow-hidden">
-                    <img
-                        :src="`/image/rcModel/${rcModel.id}`"
-                        :alt="rcModel.name"
-                        class="object-cover w-full h-full" />
+        <div class="grid grid-cols-1 mb-4 md:grid-cols-3">
+            <div
+                v-for="rcModel in rcModels"
+                class="py-10 px-5 mx-2 shadow-lg h-56 md:h-full md:py-5 rounded-[3rem]">
+                <div class="flex justify-between py-2">
+                    <Link
+                        as="h1"
+                        :href="'/rc-models/' + rcModel.id"
+                        class="max-w-full text-xl truncate md:text-4xl text-app"
+                        >{{ rcModel.name }}</Link
+                    >
                 </div>
-                <div class="flex flex-col justify-evenly">
-                    <div class="flex items-center [&>*]:mx-1">
-                        <TransmitterIcon />
-                        <p>
-                            {{ rcModel.transmitter.manufacturer + ' ' + rcModel.transmitter.name }}
-                        </p>
+                <Link as="div" :href="'/rc-models/' + rcModel.id" class="flex justify-between">
+                    <div class="aspect-square w-32 md:w-72 rounded-[2rem] md:rounded-[4rem] overflow-hidden">
+                        <img
+                            :src="`/image/rcModel/${rcModel.id}`"
+                            :alt="rcModel.name"
+                            class="object-cover w-full h-full" />
                     </div>
-                    <div class="flex items-center [&>*]:mx-1" v-if="rcModel.batteries.length > 0">
-                        <BatteryIcon />
-                        <div class="flex flex-col items-center justify-between">
-                            <p v-for="battery in rcModel.batteries">
-                                {{ battery.pivot.quantity }} x {{ battery.capacity }}
-                                {{ battery.cells }}s
+                    <div
+                        class="flex flex-col justify-evenly md:[&>div>p]:text-3xl md:[&>div>div>p]:text-3xl">
+                        <div class="flex items-center [&>*]:mx-1">
+                            <TransmitterIcon class="w-7 md:w-10" />
+                            <p>
+                                {{
+                                    rcModel.transmitter.manufacturer +
+                                    ' ' +
+                                    rcModel.transmitter.name
+                                }}
                             </p>
                         </div>
-                    </div>
-                    <div v-for="engine in rcModel.engines">
-                        <div v-if="engine.fuel" class="flex items-center [&>*]:mx-1">
-                            <GasIcon />
-                            <p>{{ engine.fuel }}</p>
+                        <div
+                            class="flex items-center [&>*]:mx-1"
+                            v-if="rcModel.batteries.length > 0">
+                            <BatteryIcon class="w-7 md:w-10" />
+                            <div class="flex flex-col items-center justify-between">
+                                <p v-for="battery in rcModel.batteries">
+                                    {{ battery.pivot.quantity }} x {{ battery.capacity }}
+                                    {{ battery.cells }}s
+                                </p>
+                            </div>
+                        </div>
+                        <div v-for="engine in rcModel.engines">
+                            <div v-if="engine.fuel" class="flex items-center [&>*]:mx-1">
+                                <GasIcon class="w-7 md:w-10" />
+                                <p>{{ engine.fuel }}</p>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </Link>
+                </Link>
+            </div>
         </div>
     </div>
 </template>
